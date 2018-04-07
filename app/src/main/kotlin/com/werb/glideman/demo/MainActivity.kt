596a683ead0c90eebfe894cb14af783f54c2485d
wanbo <werbhelius@gmail.com>
2018-04-07 12:00:57 +0800

    glide demo

--- app/src/main/kotlin/com/werb/glideman/demo/MainActivity.kt
+++ app/src/main/kotlin/com/werb/glideman/demo/MainActivity.kt
@@ -2,11 +2,22 @@ package com.werb.glideman.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522747631824&di=64639a5ee5a9f7656c62564a95d02fe8&imgtype=0&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201504%2F26%2F201504261434_tRzA4.png"
+
+        button.setOnClickListener {
+            GlideApp.with(this).load(url).into(imageView)
+        }
+
+        button2.setOnClickListener {
+            GlideApp.with(this).clear(imageView)
+        }
     }
 }
---
+++ app/src/main/kotlin/com/werb/glideman/demo/MyAppGlideModule.kt
@@ -0,0 +1,15 @@
+package com.werb.glideman.demo
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * 修改 Glide 的配置作用
+ * Created by wanbo on 2018/4/3.
+ */
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule() {
+
+
+}
